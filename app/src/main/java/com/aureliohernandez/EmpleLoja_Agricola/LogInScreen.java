764d84cdc07a6db0c8c2cf08631c17a27db89faf
hernanes338 <auherlpe@gmail.com>
2022-03-25 12:28:53 +0100

    Diseño de Splash (provisional) y Log In Screen

--- app/src/main/java/com/aureliohernandez/EmpleLoja_Agricola/LogInScreen.java
+++ app/src/main/java/com/aureliohernandez/EmpleLoja_Agricola/LogInScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,7 @@ public class LogInScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_EmpleLojaAgricola);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_screen);
 
