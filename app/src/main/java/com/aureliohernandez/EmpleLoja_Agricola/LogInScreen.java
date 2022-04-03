c52e04f642622f297f2ba4fe8b1c296b243529d5
hernanes338 <auherlpe@gmail.com>
2022-04-03 22:09:00 +0200

    Diseño de Sign Up Screen - Roles

--- app/src/main/java/com/aureliohernandez/EmpleLoja_Agricola/LogInScreen.java
+++ app/src/main/java/com/aureliohernandez/EmpleLoja_Agricola/LogInScreen.java
@@ -7,23 +7,33 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LogInScreen extends AppCompatActivity {
+    private TextView username;
+
+    private TextView password;
+
     private Button logInButton;
 
     private Button signUpActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         setTheme(R.style.Theme_EmpleLojaAgricola);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_screen);
+
+        username = (TextView) findViewById(R.id.username);
+        password = (TextView) findViewById(R.id.password);
 
         logInButton = (Button) findViewById(R.id.logInButton);
 
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // if(username.getText().toString().equals("admin"))
                 logIn();
             }
         });
